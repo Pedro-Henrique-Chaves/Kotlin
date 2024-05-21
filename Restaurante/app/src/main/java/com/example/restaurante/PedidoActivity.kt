881491421ff00c9c27055c58b1f55c5a15ac1048
Pedro-Henrique-Chaves <165905499+Pedro-Henrique-Chaves@users.noreply.github.com>
2024-05-20 21:43:48 -0300

    Update PedidoActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
@@ -5,23 +5,20 @@ import android.os.Bundle
 import com.example.restaurante.databinding.ActivityPedidoBinding
 
 class PedidoActivity : AppCompatActivity() {
-    // Declara a variável binding do tipo ActivityPedidoBinding
     private lateinit var binding: ActivityPedidoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Atribui a variável binding no ActivityPedidoBinding
         binding = ActivityPedidoBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-    // Está dizendo ao Android para usar o layout especificado pelo binding.root para exibir a interface do usuário da atividade atual
         setContentView(binding.root)
 
-        // Declara os valores passados pela intent
+        // Declara os valores passados pela intent do MainActivity
         val i = intent
-        // Extrai a quantidade de pizzas, saladas e hambúrgueres do intent
+        //ve as quantidade de pizzas, saladas e hambúrgueres do intent
         val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
         val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
         val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
 
-        // Cria uma string que contém um resumo do pedido, com informações sobre a quantidade e o preço de pizza, salada e hambúrguer.
+        // Cria uma lista que contém um resumo do pedido, com informações sobre a quantidade e o preço de pizza, salada e hambúrguer.
         val texto = "Resumo do Pedido\n" +
                 "Pizza: $quantidadePizza Preço: ${quantidadePizza*8}\n" +
                 "Salada: $quantidadeSalada Preço: ${quantidadeSalada*10}\n" +
