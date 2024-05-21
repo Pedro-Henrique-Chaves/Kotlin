38e44868c6f112576f2df02af47e0a31aa345659
Pedro-Henrique-Chaves <165905499+Pedro-Henrique-Chaves@users.noreply.github.com>
2024-05-20 21:45:11 -0300

    Update SplashActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
@@ -9,11 +9,10 @@ import android.os.Looper
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Está dizendo ao Android para usar o layout especificado pelo binding.root para exibir a interface do usuário da atividade atual.
         setContentView(R.layout.activity_splash)
-        // Cria um handler para o looper principal
+        // Cria um handler para segurar o looper principal
         Handler(Looper.getMainLooper()).postDelayed({
-            // Declara os valores passados pela intent
+            //valores passados pela intent
             val i = intent
             // Cria uma nova intent para iniciar o PedidoActivity
             val j = Intent(this,PedidoActivity::class.java)
