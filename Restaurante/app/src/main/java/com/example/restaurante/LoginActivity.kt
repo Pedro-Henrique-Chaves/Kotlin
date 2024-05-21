f91f9513de7846f37ea2f3d23bd6f96a35e6ba15
Pedro-Henrique-Chaves <165905499+Pedro-Henrique-Chaves@users.noreply.github.com>
2024-05-20 21:32:10 -0300

    Update LoginActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
@@ -7,26 +7,25 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.restaurante.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-// Criação da variável binding que servirá para manipular a tela.
+// Cria a variável binding que serve para manipular a tela.
     private lateinit var binding:ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Define o layout de Login e a atribui a variável binding.
+        // A variável binding recebe o valor do layout
         binding = ActivityLoginBinding.inflate(layoutInflater)
-       // Está dizendo ao Android para usar o layout especificado pelo binding.root para exibir a interface do usuário da atividade atual
         setContentView(binding.root)
 
-        // Define um OnClicklistener para o botão "Entrar".
+        // Define uma função para o botão "Entrar".
         binding.buttonEntrar.setOnClickListener {
 
-            // Define os valores da variável para o Username e a Senha inserida pelo usuário, converte para String e remove os espaços em branco.
+            // cria a variável Username e a Senha inserida pelo usuário, converte para String.
             val username = binding.editUsername.text.toString().trim()
             val password = binding.editPassword.text.toString().trim()
 
-            // Checa se o username e a senha inserido pelo usuário estão corretos.
-            if (username.equals("abc") && password.equals("123")) {
+            // Caso de "SE" para o username e a senha inserido pelo usuário estão corretos.
+            if (username.equals("Pedro henrique Chaves") && password.equals("21")) {
                 // Cria um Intent para iniciar o MainActivity
                 val i = Intent(this, MainActivity::class.java)
                 // Adiciona o username como extra para a intent 
@@ -37,7 +36,7 @@ class LoginActivity : AppCompatActivity() {
                 finish()
             } else {
                 // Se o usuário e a senha estiverem incorretos, exibe uma mensagem de erro.
-                Toast.makeText(applicationContext, "Errou", Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext, "Errou!!!", Toast.LENGTH_LONG).show()
             }
         }
 
